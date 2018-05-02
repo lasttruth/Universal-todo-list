8286f2ac6c113363bdca6e9f5d5dd76185a60b15
Robin Doirin <robindoirin@gmail.com>
2018-05-02 01:17:55 +0000

    basic setup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,63 @@
+class ListsController < ApplicationController
+
+  get '/users/:id' do
+      if !logged_in?
+        redirect '/lists'
+      end
+
+      @user = User.find(params[:id])
+      if @user == current_user && !@user.nil?
+        erb :'users/show'
+      else
+        redirect '/lists'
+      end
+    end
+
+    get '/signup' do
+      if !session[:user_id]
+        erb :'users/new'
+      else
+        redirect to '/tasks'
+      end
+    end
+
+    post '/signup' do
+      if params[:user_name] == "" || params[:password] == ""
+        redirect to '/signup'
+      else
+        @user = User.create(username: params[:username], password: params[:password])
+        session[:user_id] = @user.id
+        redirect "/lists"
+      end
+    end
+
+    get '/login' do
+      @error_message = params[:error]
+      if !session[:user_id]
+        erb :'users/login'
+      else
+        redirect '/lists'
+      end
+    end
+
+    post '/login' do
+      user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect "/lists"
+      else
+        redirect "/signup"
+      end
+    end
+
+    get '/logout' do
+      if session[:user_id] != nil
+        session.clear
+        redirect to '/login'
+      else
+        redirect to '/'
+      end
+    end
+
+
+  end
